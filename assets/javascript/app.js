59c1b7cc8fe0fd97b224d284df4347df48f33562
Hilton Adolinama <hadolinama@outlook.com>
2017-08-19 18:04:32 -0700

    My game

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+var quiz = TriviaQuestion.innerHTML;
+
+
+var question1 = "true";
+function Firstquestion(answer){
+	if (answer===question1) {
+		alert("You are right");
+	}else ("No, wrong response");
+}
+
+var question2 = "true";
+function Secondquestion(answer){
+	if (answer===question1) {
+		alert("You are right");
+	}else ("No, wrong response");
+
+}
+
+var question3 = "false";
+function Thirdquestion(answer){
+	if (answer===question3) {
+		alert("You are right");
+	}else ("No, wrong response");
+
+}
+
+var question4 = "false";
+function Fourthquestion(answer){
+	if (answer===question4) {
+		alert("You are right");
+	}else ("No, wrong response");
+
+}
